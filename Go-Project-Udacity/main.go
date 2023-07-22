58d1d7577e02c3541a28963ba763db5d6169ee0a
Abdirizak <70087409+Arizakmoh@users.noreply.github.com>
2023-07-22 15:23:12 +0000

    Pending changes exported from your codespace

--- Go-Project-Udacity/main.go
+++ Go-Project-Udacity/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// Create a Customer struct
+type Customer struct {
+	ID        int
+	Name      string
+	Email     string
+	Phone     string
+	Contacted bool
+}
+
 func main() {
 	fmt.Println("Test")
 }
